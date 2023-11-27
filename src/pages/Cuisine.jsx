513337944d4b53c65ad9a2b46236e2e2306658c2
wd/run <96586553+wdrun@users.noreply.github.com>
2023-11-28 00:17:33 +0800

    Update Cuisine.jsx

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -49,7 +49,7 @@ const Grid = styled(motion.div)`
 const Card = styled.div`
     img{
         display: block;
-        width: 50%;
+        width: 100%;
         border-radius: 2rem;
     }
     a{
