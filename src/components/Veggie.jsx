39e76736b175dd8d56fca27cb40f53dcfb0f05e0
wdrun <96586553+wdrun@users.noreply.github.com>
2024-02-25 16:58:00 +0800

    Update Veggie.jsx
    removed local storage function

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -20,8 +20,6 @@ function Veggie() {
     } else {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`);
       const data = await api.json();
-
-      localStorage.setItem('Veggie', JSON.stringify(data.recipes));
       setVeggie(data.recipes);
       console.log(data.recipes);
     }
