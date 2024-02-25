a7a433c73867706592b648ebca34681557b5e976
wdrun <96586553+wdrun@users.noreply.github.com>
2024-02-25 16:57:38 +0800

    Update Popular.jsx
    removed local storage function

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -21,8 +21,6 @@ function Popular() {
     } else {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
       const data = await api.json();
-
-      localStorage.setItem('Popular', JSON.stringify(data.recipes));
       setPopular(data.recipes);
       console.log(data.recipes);
     }
